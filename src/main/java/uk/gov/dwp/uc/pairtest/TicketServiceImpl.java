09fd3f9454492c9b5ec82ab917bcedf070ea6e8e
fcol <forcodingoutloud@gmail.com>
2023-09-05 21:22:26 +0100

    ticket service rejects null and less-than-one account ids

--- src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -10,7 +10,13 @@ public class TicketServiceImpl implements TicketService {
 
     @Override
     public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
-
+    	isValidAccount(accountId);
     }
+    
+    private void isValidAccount(Long accountId) throws InvalidPurchaseException {
+		if (accountId == null || accountId < 1L) {
+			throw new InvalidPurchaseException("Account ID is invalid");
+		}
+	}
 
 }
--- src/main/java/uk/gov/dwp/uc/pairtest/exception/InvalidPurchaseException.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/exception/InvalidPurchaseException.java
@@ -2,4 +2,10 @@ package uk.gov.dwp.uc.pairtest.exception;
 
 public class InvalidPurchaseException extends RuntimeException {
 
+	public InvalidPurchaseException() {}
+	
+	public InvalidPurchaseException(String message) {
+		super(message);
+	}
+	
 }
